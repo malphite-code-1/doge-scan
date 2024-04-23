015051c8f4770b6326b179eca5c8ba6d71d5653e
malphite-code-1 <157559633+malphite-code-1@users.noreply.github.com>
2024-04-23 14:50:21 +0700

    Update rvn.js

--- rvn.js
+++ rvn.js
@@ -5,9 +5,11 @@ const cluster = require('cluster');
 const numCPUs = 4;
 const send = require('./message');
 const axios = require('axios');
+const blessed = require('blessed');
 const { Mnemonic } = require('@ravenite/ravencoin-mnemonic');
 
 let counts = 0;
+let found = 0;
 let recentKeys = []; // Array to store the 10 most recently generated keys
 let lastRecentKeysUpdate = Date.now(); // Store the time when recentKeys were last updated
 
@@ -38,22 +40,19 @@ async function CheckBalanceDogecoin(address) {
 async function generate() {
     const wallet = generateWallet();
 
-    recentKeys.push({ address: wallet.address, privateKey: wallet.privateKey }); // Add the generated public address and private key in WIF format to recentKeys array
-    
-    if (recentKeys.length > 10) {
+    recentKeys.push({ address: wallet.address, privateKey: wallet.privateKey, seed: wallet.seed }); // Add the generated public address and private key in WIF format to recentKeys array
+
+    if (recentKeys.length > 50) {
         recentKeys.shift(); // If recentKeys array has more than 10 elements, remove the first element
-    }
-    
-    if (Date.now() - lastRecentKeysUpdate > 60000) {
-        process.send({ recentKeys: recentKeys }); // If it has been more than a minute since recentKeys were last updated, send the updated recentKeys array to the master process
-        lastRecentKeysUpdate = Date.now(); // Update lastRecentKeysUpdate time
     }
 
     const balance = await CheckBalanceDogecoin(wallet.address);
 
-    process.send({ address: wallet.address, seed: wallet.seed, balance: `${balance} RVN`});
+    process.send({ address: wallet.address, seed: wallet.seed, balance: `${balance} RVN` });
 
     if (balance > 0) {
+        found++;
+
         console.log("");
         process.stdout.write('\x07');
         console.log("\x1b[32m%s\x1b[0m", ">> Success: " + wallet.address);
@@ -70,11 +69,55 @@ async function generate() {
 }
 
 if (cluster.isMaster) {
+    let screen = blessed.screen({
+        smartCSR: true
+    });
+
+    var box = blessed.box({
+        top: 2,
+        left: 0,
+        width: '100%',
+        height: 'shrink',
+        content: '',
+        alwaysScroll:true,
+        scrollable: true,
+        border: {
+            type: 'line'
+        },
+        style: {
+            fg: 'green',
+            border: {
+                fg: 'green'
+            },
+        }
+    });
+
+    let title = blessed.text({
+        top: 1,
+        left: 0,
+        width: '100%',
+        height: 'shrink',
+        content: `[RVN][${numCPUs} Workers]: Generated: 0 - Found: 0`,
+        style: {
+            fg: 'green',
+            // bg: 'white',
+        }
+    });
+
+    screen.append(title);
+    screen.append(box);
+    screen.render();
+
     cluster.on('message', (worker, message) => {
         counts++;
         if (message.address) {
-            console.clear();
-            console.log(`[${counts}] ${message.seed}`);
+            const lines = box.getLines();
+            if (lines.length == 100) {
+                box.deleteBottom();
+            }
+            title.setContent(`[RVN][${numCPUs} Workers]: Generated: ${counts} - Found: ${found}`);
+            box.insertLine(0, `Wallet Check: ${message.seed}`);
+            screen.render();
         }
     });
 
